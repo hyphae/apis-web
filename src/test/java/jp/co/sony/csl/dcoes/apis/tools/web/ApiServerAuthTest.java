2f6cc2b7e612826c60b2ae106778f6cda2a01f6e
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-07-03 19:06:03 +0100

    Add API key authentication to ApiServer with unit tests
    Signed-off-by: Chukwuemeka David <david.chukwuemeka.swe@gmail.com>

---
+++ src/test/java/jp/co/sony/csl/dcoes/apis/tools/web/ApiServerAuthTest.java
@@ -0,0 +1,43 @@
+package jp.co.sony.csl.dcoes.apis.tools.web;
+
+import io.vertx.core.http.HttpServerRequest;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+
+public class ApiServerAuthTest {
+
+    @Test
+    public void nullApiKey_returns500() {
+        HttpServerRequest req = mock(HttpServerRequest.class);
+        assertEquals(Integer.valueOf(500), ApiServer.checkAuth(req, null));
+    }
+
+    @Test
+    public void emptyApiKey_returns500() {
+        HttpServerRequest req = mock(HttpServerRequest.class);
+        assertEquals(Integer.valueOf(500), ApiServer.checkAuth(req, ""));
+    }
+
+    @Test
+    public void missingHeader_returns401() {
+        HttpServerRequest req = mock(HttpServerRequest.class);
+        when(req.getHeader("X-API-Key")).thenReturn(null);
+        assertEquals(Integer.valueOf(401), ApiServer.checkAuth(req, "secret"));
+    }
+
+    @Test
+    public void wrongKey_returns401() {
+        HttpServerRequest req = mock(HttpServerRequest.class);
+        when(req.getHeader("X-API-Key")).thenReturn("wrong");
+        assertEquals(Integer.valueOf(401), ApiServer.checkAuth(req, "secret"));
+    }
+
+    @Test
+    public void correctKey_returnsNull() {
+        HttpServerRequest req = mock(HttpServerRequest.class);
+        when(req.getHeader("X-API-Key")).thenReturn("secret");
+        assertNull(ApiServer.checkAuth(req, "secret"));
+    }
+}
