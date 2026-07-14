5bb304fa2d3ae0e2031de752e6f35714fb255f18
Chukwuemeka David <david.chukwuemeka.swe@gmail.com>
2026-07-14 19:53:30 +0100

    refactor: replace wildcard imports with explicit named imports
    Signed-off-by: Chukwuemeka David <david.chukwuemeka.swe@gmail.com>

--- src/test/java/jp/co/sony/csl/dcoes/apis/tools/web/ApiServerAuthTest.java
+++ src/test/java/jp/co/sony/csl/dcoes/apis/tools/web/ApiServerAuthTest.java
@@ -2,8 +2,10 @@ package jp.co.sony.csl.dcoes.apis.tools.web;
 
 import io.vertx.core.http.HttpServerRequest;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class ApiServerAuthTest {
